75c2770b045aabc2f4e600496de4c2a9338e8851
sibelephant <masterpiekhalid@gmail.com>
2025-08-02 02:48:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Express + TypeScript Scaffold
+
+A minimal starting point for building Express apps with TypeScript.
+
+## Getting Started
+
+Install dependencies:
+
+```bash```
+npm install
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("vote", (optionId: string) => {
+    io.emit("updateVotes", {
+      /*updated vote data */
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+// Middlewares
+app.use(express.json());
+
+// Routes
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
